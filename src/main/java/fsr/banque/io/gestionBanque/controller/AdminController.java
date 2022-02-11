db54d5ef17eb83f12277bba081d04ded9e564a8e
larhchim <larhchim.i.fst@uhp.ac.ma>
2022-02-11 20:03:52 +0100

    bug fix name Controller Layer

--- src/main/java/fsr/banque/io/gestionBanque/controller/AdminController.java
+++ src/main/java/fsr/banque/io/gestionBanque/controller/AdminController.java
@@ -1,8 +1,9 @@
 package fsr.banque.io.gestionBanque.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class AdminController {
 
 }
